06fed63877e823ba5fdd8288a278e315d9e001b3
Ivan Tonchev <tonchev.b.ivan@gmail.com>
2022-10-07 23:58:58 +0300

    enable gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+    token: 'ETH',
+    gasPrice: 20,
+    gasPriceApi: 'https://api.etherscan.io/api?module=proxy&action=eth_gasPrice',
+  },
 };
