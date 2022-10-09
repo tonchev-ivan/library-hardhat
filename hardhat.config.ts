9a5f0b54d20bc646e73cf145b63c73a40a6ca1fc
Ivan Tonchev <tonchev.b.ivan@gmail.com>
2022-10-09 10:29:59 +0300

    update travis config
    hide coinmarketcap token

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,7 @@ module.exports = {
     token: 'ETH',
     gasPrice: 20,
     gasPriceApi: 'https://api.etherscan.io/api?module=proxy&action=eth_gasPrice',
-    coinmarketcap: 'aace7e05-66c0-42c5-87ec-57adc3953e49',
+    coinmarketcap: process.env.COINMARKETCAP_TOKEN,
+    maxMethodDiff: 25,
   },
 };
