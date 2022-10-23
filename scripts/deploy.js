7dafdf6605e6a7bce04e8a33951a6c0a38bccf19
Ivan Tonchev <tonchev.b.ivan@gmail.com>
2022-10-23 15:46:05 +0300

    include coverage report into travis build

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,6 @@
 async function main() {
-  // await (await hre.ethers.getContractFactory("Library")).deploy();
-
   const Library = await ethers.getContractFactory("Library");
   const library = await Library.deploy();
-
 
   console.log(
     'Library deployed',
@@ -12,8 +9,6 @@ async function main() {
   console.log("Token address:", library.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
--- scripts/verify.js
+++ scripts/verify.js
@@ -6,8 +6,8 @@ async function main() {
     });
 }
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
