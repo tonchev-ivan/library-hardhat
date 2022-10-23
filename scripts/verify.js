74ca6f349bb38f235584a11855ebdbf8cc78bd4f
Ivan Tonchev <tonchev.b.ivan@gmail.com>
2022-10-23 15:33:26 +0300

    add verification task

---
+++ scripts/verify.js
@@ -0,0 +1,13 @@
+const hre = require("hardhat");
+async function main() {
+
+    await hre.run("verify:verify", {
+        address: process.env.CONTRACT_ADDRESS,
+    });
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
